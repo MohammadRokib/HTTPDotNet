4286e7f67fdeeda460413e4f6f89a065a95f24c7
MohammadRokib <rokib.khan@leads-bd.com>
2025-09-17 10:27:37 +0600

    Add project files.

---
+++ src/HttpNet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public class HttpNet
+    {
+        private string FilePath = String.Empty;
+
+        public HttpNet()
+        {
+            FilePath = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.ToString();
+            FilePath = Path.Combine(FilePath, "src", "messages.txt");
+        }
+        public void Read()
+        {
+            if (!File.Exists(FilePath))
+            {
+                Console.WriteLine("File doesn't exist");
+                return;
+            }
+
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+                int charCode;
+                while ((charCode = sr.Read()) != -1)
+                {
+                    Console.WriteLine((char)charCode);
+                }
+
+            }
+        }
+    }
+}
